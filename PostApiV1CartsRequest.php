059a529b575b1358eeee43dfa3bb0aa83d599d91
Ronald Dávila Gallardo <46361976+rondg13@users.noreply.github.com>
2021-05-14 14:28:36 -0500

    Validators
    Post and Put requests validators

---
+++ PostApiV1CartsRequest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Requests\Api\V1\Carts;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostApiV1CartsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'storeId' => 'required|integer',
+            'currency' => 'required|string',
+            'products' => 'nullable|array',
+            'products.*.id' => 'nullable',
+            'products.*.variantId' => 'nullable',
+            'products.*.quantity' => 'nullable',
+            'products.*.tbyb' => 'nullable',
+            'products.*.price' => 'nullable',
+            'products.*.originalPrice' => 'nullable',
+            'products.*.thumbnail' => 'nullable',
+            'products.*.url' => 'nullable',
+            'products.*.weight' => 'nullable',
+            'products.*.options' => 'nullable'
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'storeId.required' => 'This field is required',
+            'currency.required' => 'A currency type is required'
+        ];
+    }
+}
